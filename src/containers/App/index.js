9b9e18ffe29d344ca51ce41f3b6dad7a37e49d4e
allisonkydy <allisnelly@gmail.com>
2019-11-14 17:22:23 -0600

    profile page secured with private route

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
-// import { useAuth0 } from '../../react-auth0-spa';
+import { useAuth0 } from '../../react-auth0-spa';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Profile from '../../components/Profile';
+import PrivateRoute from '../../components/PrivateRoute';
 
 function App() {
-  // const { loading } = useAuth0();
+  const { loading } = useAuth0();
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="App">
@@ -19,7 +20,7 @@ function App() {
         </header>
         <Switch>
           <Route path="/" exact />
-          <Route path="/profile" component={Profile} />
+          <PrivateRoute path="/profile" component={Profile} />
         </Switch>
       </BrowserRouter>
     </div>
