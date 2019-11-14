9489b6697ebeef3698369b78e870ed92d6217185
allisonkydy <allisnelly@gmail.com>
2019-11-14 17:08:23 -0600

    reading and displaying user profile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { useAuth0 } from '../react-auth0-spa';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -13,6 +14,13 @@ const NavBar = () => {
       )}
 
       {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+      {isAuthenticated && (
+        <span>
+          <Link to="/">Home</Link>&nbsp;
+          <Link to="/profile">Profile</Link>
+        </span>
+      )}
     </div>
   );
 };
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
-import { useAuth0 } from '../../react-auth0-spa';
+// import { useAuth0 } from '../../react-auth0-spa';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Profile from '../../components/Profile';
 
 function App() {
-  const { loading } = useAuth0();
+  // const { loading } = useAuth0();
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
