5b7836ca26ec29c8ea9f1f81f89c3b676f816e33
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-18 16:38:07 -0600

    convert auth0 config to js

---
+++ src/utils/auth-config.js
@@ -0,0 +1,11 @@
+// {
+//   "domain": "moodmuse.auth0.com",
+//   "clientId": "RWYKn1NSBG6LjReho2WItTjxxdtBxjXj",
+//   "audience": "http://localhost:5000"
+// }
+
+const AUTH0_DOMAIN = 'moodmuse.auth0.com';
+const AUTH0_CLIENT_ID = 'RWYKn1NSBG6LjReho2WItTjxxdtBxjXj';
+const AUTH0_AUDIENCE = 'http://localhost:5000';
+
+export { AUTH0_CLIENT_ID, AUTH0_AUDIENCE, AUTH0_DOMAIN };
