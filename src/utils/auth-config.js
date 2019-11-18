4d56b537affc7f73969beff96939e50e13be0dad
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-18 17:59:01 -0600

    update auth0 config

--- src/utils/auth-config.js
+++ src/utils/auth-config.js
@@ -1,5 +1,19 @@
-const AUTH0_DOMAIN = 'moodmuse.auth0.com';
-const AUTH0_CLIENT_ID = 'RWYKn1NSBG6LjReho2WItTjxxdtBxjXj';
-const AUTH0_AUDIENCE = 'http://localhost:5000';
+/* eslint-disable import/no-mutable-exports */
+let AUTH0_CLIENT_ID;
+let AUTH0_DOMAIN;
+let AUTH0_AUDIENCE;
+if (process.env.NODE_ENV === 'development') {
+  AUTH0_AUDIENCE = 'http://localhost:5000';
+  AUTH0_CLIENT_ID = 'oVhKrQPd2E4o0n9mPPtV1rfoKJaiMYNi';
+  AUTH0_DOMAIN = 'moodmuse-dev.auth0.com';
+} else if (process.env.NODE_ENV === 'staging') {
+  AUTH0_AUDIENCE = 'https://moodmuse-staging.herokuapp.com';
+  AUTH0_CLIENT_ID = 'KwOV32dmJl1hXtMWAkaf8AgV4NNjYdPW';
+  AUTH0_DOMAIN = 'moodmuse-staging.auth0.com';
+} else if (process.env.NODE_ENV === 'production') {
+  AUTH0_AUDIENCE = 'https://moodmuse.herokuapp.com';
+  AUTH0_CLIENT_ID = 'RWYKn1NSBG6LjReho2WItTjxxdtBxjXj';
+  AUTH0_DOMAIN = 'moodmuse.auth0.com';
+}
 
 export { AUTH0_CLIENT_ID, AUTH0_AUDIENCE, AUTH0_DOMAIN };
