19a8e36d5ca521ba5771d4c851a479acc560daca
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-18 16:39:34 -0600

    fix linting errors

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,5 @@
 // src/components/Profile.js
-
-import React, { Fragment } from 'react';
+import React from 'react';
 import { useAuth0 } from '../react-auth0-spa';
 
 const Profile = () => {
@@ -10,16 +9,14 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
 
-  console.log(user);
-
   return (
-    <Fragment>
+    <>
       <img src={user.picture} alt="Profile" />
 
       <h2>{user.name}</h2>
       <p>{user.email}</p>
       <code>{JSON.stringify(user, null, 2)}</code>
-    </Fragment>
+    </>
   );
 };
 
