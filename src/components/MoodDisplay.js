4361d930ae09534ebe41657486400bffcd8e381b
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-18 16:41:01 -0600

    get user data from auth0 hook

--- src/components/MoodDisplay.js
+++ src/components/MoodDisplay.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { getMoodsQuery } from '../queries';
 import { useQuery } from '@apollo/react-hooks';
+import { checkForUserAndGetMoodsQuery } from '../queries';
+import { useAuth0 } from '../react-auth0-spa';
 
-function MoodDisplay({ sub }) {
-  const { loading, error, data } = useQuery(getMoodsQuery, {
-    variables: { sub: sub },
+function MoodDisplay() {
+  const { user } = useAuth0();
+  const { loading, error, data } = useQuery(checkForUserAndGetMoodsQuery, {
+    variables: { sub: user.sub, email: user.email },
   });
 
-  console.log('data: ', data);
-
-  return <code>{JSON.stringify(data, null, 2)}</code>;
+  // console.log('data: ', data);
+  // console.log('user: ', user);
+  if (loading) return <p>Loading Moods...</p>;
+  if (error) return <p>Error fetching moods.</p>;
+  return (
+    <>
+      <code>{JSON.stringify(data, null, 2)}</code>
+    </>
+  );
 }
 
 export default MoodDisplay;
