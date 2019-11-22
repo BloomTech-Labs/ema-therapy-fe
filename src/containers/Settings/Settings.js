d58381011d3ee3615094e6672c7140bf21899876
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-22 12:12:11 -0600

    pass settings components to Dashboard

--- src/containers/Settings/Settings.js
+++ src/containers/Settings/Settings.js
@@ -1,22 +1,62 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '../../utils/react-auth0-spa';
-import Logout from '../../components/Logout';
+import Logout from './Logout';
+import Dashboard from '../Dashboard';
 
 const Settings = () => {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <div>
-      SETTINGS
-      {isAuthenticated && (
-        <div>
-          <Link to="/profile">Profile</Link>
+    <Dashboard>
+      <StyledSettings>
+        <h1>Settings</h1>
+        <div className="setting-group">
+          <h2 className="setting-group__heading">Preferences</h2>
+          <div className="setting-group__item">Share My Location</div>
+          <div className="setting-group__item">Dark Mode</div>
         </div>
-      )}
-      <Logout />
-    </div>
+        {isAuthenticated && <StyledLink to="/profile">Profile</StyledLink>}
+        <Logout />
+      </StyledSettings>
+    </Dashboard>
   );
 };
 
 export default Settings;
+
+const StyledSettings = styled.div`
+  h1 {
+    font-size: 24px;
+    line-height: 30px;
+    padding: 20px 0;
+    font-weight: normal;
+  }
+
+  .setting-group {
+    margin-bottom: 20px;
+  }
+
+  .setting-group__heading {
+    font-size: 15px;
+    font-weight: normal;
+    margin: 0 0 3px;
+  }
+
+  .setting-group__item {
+    height: 34px;
+    background: #c4c4c4;
+    border-radius: 5px;
+    padding-left: 10px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 6px;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  /* font-size: 15px; */
+  color: #000;
+  text-decoration: none;
+`;
