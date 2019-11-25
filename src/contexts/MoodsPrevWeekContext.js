04469a28a58e0d99207e62f0e68d777cfd785713
allisonkydy <allisnelly@gmail.com>
2019-11-25 16:56:38 -0600

    create moodsPrevWeek context and provider

---
+++ src/contexts/MoodsPrevWeekContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react';
+
+export const MoodsPrevWeekContext = createContext({
+  moods: [],
+  setMoods: () => {},
+});
+
+export const MoodsPrevWeekProvider = ({ children }) => {
+  const [state, setState] = useState({
+    moods: [],
+    setMoods: (moods) => {
+      setState({ ...state, moods });
+    },
+  });
+
+  return (
+    <MoodsPrevWeekContext.Provider value={state}>
+      {children}
+    </MoodsPrevWeekContext.Provider>
+  );
+};
