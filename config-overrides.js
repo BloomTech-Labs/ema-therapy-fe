e79598d81ccadbc5e91fed167ad9123e82bf1f48
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-02 13:47:40 -0600

    adds comments to document overrides

--- config-overrides.js
+++ config-overrides.js
@@ -4,18 +4,23 @@ const path = require('path');
 // https://ant.design/docs/react/use-with-create-react-app#Advanced-Guides
 
 module.exports = override(
+  // https://ant.design/docs/react/use-with-create-react-app#Use-babel-plugin-import
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: true,
   }),
+  // https://ant.design/docs/react/use-with-create-react-app#Customize-Theme
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: {
       // override any defaults from https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+      // must restart the server for changes to take effect
       '@primary-color': '#1DA57A',
     },
   }),
+  // reduces bundle size significantly by aliasing antd's icon directory to
+  // `./src/icons.js` where antd icons can be individually exported
   // https://github.com/ant-design/ant-design/issues/12011#issuecomment-552117531
   // eslint-disable-next-line func-names
   function(config) {
