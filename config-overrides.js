61e3cccbb38273e351a02b639a1c3b61f8c07fee
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-02 11:07:19 -0600

    adds less and less-loader as dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,17 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      // override any defaults from...
+      // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+      '@primary-color': '#BADA55',
+    },
   }),
 );
