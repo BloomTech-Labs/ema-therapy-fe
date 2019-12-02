fe4894d048c5ed421ac8e801c8ee68b1b0f632c4
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-01 19:12:30 -0600

    adds https api call and returns a cleanup function

--- src/hooks/useCurrentWeather.js
+++ src/hooks/useCurrentWeather.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Avatar } from 'antd';
 
 const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
@@ -11,18 +12,22 @@ const useCurrentWeather = () => {
   const [currentWeather, setCurrentWeather] = useState(null);
 
   useEffect(() => {
+    const ac = new AbortController();
+    const { signal } = ac;
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(async (position) => {
         const { latitude, longitude } = position.coords;
         // Get the user's current weather from Open Weather API
-        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`;
-        const res = await fetch(url);
+        const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`;
+        const res = await fetch(WEATHER_API, { signal });
         const data = await res.json();
         const temp = convertTempFromKelvinToFahr(data.main.temp);
         const description = data.weather[0].main;
         setCurrentWeather(`${description} ${temp}°`);
       });
     }
+
+    return () => ac.abort();
   }, []);
   return currentWeather;
 };
