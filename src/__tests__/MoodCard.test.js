91b805bd1d652269a69ca2dca8858261c5d9d533
allisonkydy <allisnelly@gmail.com>
2019-12-04 14:58:59 -0600

    refactored mood card test

--- src/__tests__/MoodCard.test.js
+++ src/__tests__/MoodCard.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import MoodCard from '../components/MoodCard';
 
 test('MoodCard renders correctly', () => {
-  const tree = renderer
-    .create(
+  expect(
+    render(
       <MoodCard
         mood={{
           mood: 1,
@@ -16,8 +16,6 @@ test('MoodCard renders correctly', () => {
           weather: 'mockWeather',
         }}
       />,
-    )
-    .toJSON();
-
-  expect(tree).toMatchSnapshot();
+    ),
+  ).toMatchSnapshot();
 });
