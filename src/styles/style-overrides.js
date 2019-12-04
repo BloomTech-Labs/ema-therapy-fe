3873b7a4f4045656c5722e83165947366b9c45e1
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-03 19:54:19 -0600

    add a comment linking to the default vars

--- src/styles/style-overrides.js
+++ src/styles/style-overrides.js
@@ -1,3 +1,8 @@
+/*
+  These styles used in '../../config-overrides.js to override any defaults from
+  https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+  You must restart the server for changes to take effect. */
+
 module.exports = {
   '@font-family':
     "'Muli',-apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang SC', 'Hiragino Sans GB','Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif",
