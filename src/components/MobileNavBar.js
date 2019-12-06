88175542631a5d9022160fac21822aab0ef9f11b
amlane <twelvexstring@gmail.com>
2019-12-06 17:58:25 -0600

    removed unused import

--- src/components/MobileNavBar.js
+++ src/components/MobileNavBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Icon } from 'antd';
 import { Link, NavLink } from 'react-router-dom';
 import styles from '../styles/theme';
 import {
