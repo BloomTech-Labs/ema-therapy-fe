9650c8899b169781bb59f6571c72af5fe95a95e9
allisonkydy <allisnelly@gmail.com>
2019-12-06 19:34:26 -0600

    edited layout & styles on welcome page (still need image)

--- src/containers/Welcome/Welcome.js
+++ src/containers/Welcome/Welcome.js
@@ -3,13 +3,20 @@ import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAuth0 } from '../../utils/react-auth0-spa';
 import Login from '../../components/Login';
+import styles from '../../styles/theme';
 
 const Welcome = () => {
   const { isAuthenticated } = useAuth0();
   return (
     <StyledWelcome>
-      WELCOME
-      <Login />
+      <h2>MoodBloom</h2>
+      <LoginWrapper>
+        <div className="slogan-text">
+          <h1>Mood Tracker</h1>
+          <p>Flourish into a healthier, happier you</p>
+        </div>
+        <Login />
+      </LoginWrapper>
       {isAuthenticated && <Redirect to="/dashboard" />}
     </StyledWelcome>
   );
@@ -19,9 +26,32 @@ const StyledWelcome = styled.div`
   max-width: 500px;
   height: 100vh;
   margin: 0 auto;
-  border: 2px solid black;
-  text-align: center;
-  padding: 25%;
+  padding: 30px 20px 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: ${styles.darkJungleGreen};
+  color: white;
+
+  h2 {
+    color: white;
+    font-weight: 600;
+    font-size: 18px;
+    padding-left: 7px;
+  }
+`;
+
+const LoginWrapper = styled.div`
+  .slogan-text {
+    padding: 0 17px;
+  }
+
+  h1 {
+    color: white;
+    margin-bottom: 6px;
+    font-weight: 600;
+    font-size: 24px;
+  }
 `;
 
 export default Welcome;
