e46b499ab3aacd1716e0e60d7939a0b22dedf5fa
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-18 10:06:17 -0600

    hook up back button to router

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,14 +5,14 @@ import NotFound from '../containers/NotFound/404';
 
 function Task() {
   const { task } = useParams();
+  const history = useHistory();
   if (!(task >= 1 && task <= 7)) return <NotFound />;
-
   return (
     <>
       <Icon
         type="left"
         style={{ fontSize: 22, color: '#9cd9dd' }}
-        onClick={() => console.log('go back')}
+        onClick={() => history.goBack()}
       />
       <h1>Task {task}</h1>
     </>
