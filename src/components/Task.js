506e6605b42920e917e541897018fc94bb4f39ed
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-18 10:10:50 -0600

    add some basic container styles

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Icon } from 'antd';
 import { useParams, useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 import NotFound from '../containers/NotFound/404';
+import styles from '../styles/theme';
 
 function Task() {
   const { task } = useParams();
   const history = useHistory();
   if (!(task >= 1 && task <= 7)) return <NotFound />;
   return (
-    <>
+    <TaskWrapper>
       <Icon
         type="left"
         style={{ fontSize: 22, color: '#9cd9dd' }}
         onClick={() => history.goBack()}
       />
       <h1>Task {task}</h1>
-    </>
+    </TaskWrapper>
   );
 }
 
 export default Task;
+
+const TaskWrapper = styled.div`
+  background-color: ${styles.seafoamGreen};
+  height: 100%;
+  padding: 30px 25px;
+`;
