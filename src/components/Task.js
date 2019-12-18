f9bf7a17024ef979a652cb4193b66b13325cfcc1
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-18 10:04:21 -0600

    add back button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Icon } from 'antd';
+import { useParams, useHistory } from 'react-router-dom';
 import NotFound from '../containers/NotFound/404';
 
 function Task() {
   const { task } = useParams();
   if (!(task >= 1 && task <= 7)) return <NotFound />;
 
-  return <h1>Task {task}</h1>;
+  return (
+    <>
+      <Icon
+        type="left"
+        style={{ fontSize: 22, color: '#9cd9dd' }}
+        onClick={() => console.log('go back')}
+      />
+      <h1>Task {task}</h1>
+    </>
+  );
 }
 
 export default Task;
