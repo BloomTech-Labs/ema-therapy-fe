1458ebe37a1d5737f70ae1ebe326666b617cccd6
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-18 21:01:20 -0600

    ios notification instructions for adding to home screen

--- src/components/AppleNotification.js
+++ src/components/AppleNotification.js
@@ -1,29 +1,55 @@
-import React from 'react';
-
-// Detects if device is on iOS, and notifies user to download PWA
+import React, { useState, useEffect } from 'react';
+import { Modal } from 'antd';
+import { IoIosShare } from 'react-icons/io';
+import useStandalone from '../hooks/useStandalone';
+import theme from '../styles/theme';
 
 function AppleNotification() {
-  const isIos = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
+  const isStandalone = useStandalone();
+
+  const showModal = () => {
+    let showIosNotification;
+    if (window.localStorage.getItem('showIosNotification') !== null) {
+      showIosNotification = false;
+    } else {
+      showIosNotification = true;
+    }
+    return showIosNotification;
   };
 
-  const isInStandaloneMode = () =>
-    'standalone' in window.navigator && window.navigator.standalone;
+  const [isModalOpen, setIsModalOpen] = useState(showModal);
 
-  return (
-    <>
-      {isIos() && !isInStandaloneMode() ? (
-        <div className="mobile-alert ios-download">
-          <p>
-            For a better experience, open this page in Safari, press the Action
-            menu at the bottom center and press{' '}
-            <strong>Add to Home Screen</strong>. Enjoy!
-          </p>
-        </div>
-      ) : null}
-    </>
-  );
+  const ua = window.navigator.userAgent;
+  const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+  const webkit = !!ua.match(/WebKit/i);
+  const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+
+  return !isStandalone && iOS ? (
+    <Modal
+      visible={isModalOpen}
+      closable={false}
+      cancelText="Don't show this again"
+      onOk={() => setIsModalOpen(false)}
+      onCancel={() => {
+        window.localStorage.setItem('showIosNotification', false);
+        setIsModalOpen(false);
+      }}
+    >
+      <h3 style={{ color: theme.darkJungleGreen }}>
+        Add MoodBloom to Home Screen
+      </h3>
+      <p>
+        For a better experience,{' '}
+        {!iOSSafari && (
+          <>
+            <strong>open this page in Safari,</strong>{' '}
+          </>
+        )}{' '}
+        press the share button <IoIosShare size={24} /> and choose{' '}
+        <strong>Add to Home Screen</strong>.
+      </p>
+    </Modal>
+  ) : null;
 }
 
 export default AppleNotification;
