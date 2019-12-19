5d7a6e1e33bf2acb8dcddfd61e86f2aa7603fed2
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-18 21:02:18 -0600

    remove unused useEffect import

--- src/components/AppleNotification.js
+++ src/components/AppleNotification.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Modal } from 'antd';
 import { IoIosShare } from 'react-icons/io';
 import useStandalone from '../hooks/useStandalone';
