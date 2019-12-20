93067a7b7556a88543af381033ed2678d5aad7be
amlane <twelvexstring@gmail.com>
2019-12-20 12:33:08 -0600

    removed unused imports from private route and task

--- src/components/DailyTask/Task.js
+++ src/components/DailyTask/Task.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Icon, Input } from 'antd';
 import { useMutation } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
