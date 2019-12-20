6bd0964549f4fac6fec26bde79831d6bc973d215
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-12-20 11:34:01 -0800

    update chart styles, add prop types FOR JONATHAN

--- src/components/ChartViews/PieLegend.js
+++ src/components/ChartViews/PieLegend.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import PieSlice from './PieSlice';
 
 const PieLegend = ({ totalMoods }) => {
   return (
     <PieLegendWrapper>
       {totalMoods.map((sliceData) => {
-        return <PieSlice sliceData={sliceData} />;
+        return <PieSlice sliceData={sliceData} key={sliceData.color} />;
       })}
     </PieLegendWrapper>
   );
@@ -17,5 +18,15 @@ const PieLegendWrapper = styled.div`
   justify-content: space-evenly;
   margin-top: 20px;
 `;
+
+PieLegend.propTypes = {
+  totalMoods: PropTypes.arrayOf(
+    PropTypes.shape({
+      color: PropTypes.string,
+      mood: PropTypes.string,
+      percent: PropTypes.number,
+    }),
+  ).isRequired,
+};
 
 export default PieLegend;
