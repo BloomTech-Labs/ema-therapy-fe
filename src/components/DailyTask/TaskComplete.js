51406dc9347d8526fb4bf76b45de532b216d0dbe
amlane <twelvexstring@gmail.com>
2019-12-20 12:42:07 -0600

    nvm that's not right

--- src/components/DailyTask/TaskComplete.js
+++ src/components/DailyTask/TaskComplete.js
@@ -9,7 +9,6 @@ function TaskComplete() {
   return (
     <Dashboard>
       <Confetti
-        width={500}
         height={window.innerHeight - 58}
         colors={[
           styles.brightYellow,
