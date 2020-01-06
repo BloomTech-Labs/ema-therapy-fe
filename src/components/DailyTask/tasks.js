d2ee931c78a854b9cc99e79f616a1d85146ded75
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-01-06 15:23:29 -0600

    add daily tasks and prompts

---
+++ src/components/DailyTask/tasks.js
@@ -0,0 +1,39 @@
+const tasks = [
+  {
+    task: 'Worries',
+    prompt:
+      'Without stopping write down everything that is churning around in your mind. Once it is out of your head, give yourself permission to leave it here and come back to it later with a clearer head.',
+  },
+  {
+    task: 'Reflection',
+    prompt:
+      'Sit quietly for a few minutes, just breathing and listening to your body before you start writing. What is it telling you? Where do you feel strong, healthy, energetic, relaxed? Where do you feel tense, worried, uncomfortable, tired? What is asking for more attention?',
+  },
+  {
+    task: 'One Word',
+    prompt:
+      'Pick a single positive word that you want to focus on today – such as joy, gratitude, love or courage. Journal about all the ways you have experienced this word lately and all the ways you want to.',
+  },
+  {
+    task: 'Victories',
+    prompt:
+      'Write a list of your recent accomplishments and victories, no matter how small. Celebrate each one individually.',
+  },
+  {
+    task: 'Habits',
+    prompt:
+      'Write about a habit or activity you do that improves your mood or stress level without fail. Describe how you transform when you are engaged in this practice. How do you feel? How can you do this more often?',
+  },
+  {
+    task: 'Lyrics',
+    prompt:
+      'Think about the lyrics to a song that makes you happy.  What is it about this song that makes you feel this way.',
+  },
+  {
+    task: 'Gratitude',
+    prompt:
+      'Take a second to reflect on something in your life that you are grateful for right now. Write about why you feel that way.',
+  },
+];
+
+export default tasks;
